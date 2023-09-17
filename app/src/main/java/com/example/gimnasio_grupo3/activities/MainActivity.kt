bfc1176f378347575e111ad3e74b1b98c7a40d73
Gino Basile <basilegino@hotmail.com>
2023-09-17 18:41:11 -0300

    update menu

--- app/src/main/java/com/example/gimnasio_grupo3/activities/MainActivity.kt
+++ app/src/main/java/com/example/gimnasio_grupo3/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.gimnasio_grupo3.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.gimnasio_grupo3.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottonNavView : BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        bottonNavView = findViewById(R.id.bottom_bar)
+
+        NavigationUI.setupWithNavController(bottonNavView, navHostFragment.navController)
     }
 }
